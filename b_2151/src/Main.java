4c91af651c7910aedd49ce95b193ddd11225a35b
minju <minju9711@naver.com>
2025-03-05 16:38:03 +0900

    [add] b_1051

--- b_2151/src/Main.java
+++ b_2151/src/Main.java
@@ -1,54 +1,87 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-    static char room [][];
-    static int dirX[] = {-1,0,1,0}; //방향은 인덱스 1부터 계산
-    static int dirY[] = {0,1,0,-1};
-    static int answer = Integer.MAX_VALUE;
-    static boolean [][][] minRoute;
 
+    static int [][][] dp;
+    static char [][] house;
+    static int [] dirX = {0,0,-1,1};
+    static int [] dirY = {-1,1,0,0};
+    static int N;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        room = new char[N+2][N+2];
-        minRoute = new boolean[4][N+2][N+2];
+        N = Integer.parseInt(br.readLine());
 
+        dp = new int[4][N][N];
+        house = new char[N][N];
         int startX=0;
         int startY=0;
-        for(int i=1; i<=N; i++) {
+
+        for(int i=0; i<N; i++){
             String s = br.readLine();
-            for(int j=1; j<=N; j++) {
-                room[i][j] = s.charAt(j-1);
-                if(room[i][j]=='#'){
+            house[i]=s.toCharArray();
+            for(int j=0; j<N; j++){
+                if(house[i][j]=='#'){
                     startX=i;
                     startY=j;
                 }
             }
         }
 
-        room[startX][startY]='*';
-        findMirror(startX, startY,0,-1);
+        // DP 배열을 최댓값으로 초기화
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < N; j++) {
+                Arrays.fill(dp[i][j], Integer.MAX_VALUE);
+            }
+        }
+
+        Queue<int[]> queue = new ArrayDeque<>();
+
+        for(int i=0; i<4; i++){
+            int nextX = startX+dirX[i];
+            int nextY = startY+dirY[i];
+
+            if(nextX<0||nextY<0||nextX>=N||nextY>=N)continue;
+            if(house[nextX][nextY]=='*')continue;
+
+            dp[i][nextX][nextY] = 0;
+            queue.add(new int[]{nextX, nextY, i, 0}); // (x, y, 방향, 거울 개수)
+        }
+
+        int answer = Integer.MAX_VALUE;
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
+            int nowX = now[0];
+            int nowY = now[1];
+            int nowDir = now[2];
+            int nowMirror = now[3];
+            int reverseDir = (nowDir+2)%4;
+            if (house[nowX][nowY] == '#') {
+                answer = Math.min(answer, nowMirror);
+                continue;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int nextX = nowX + dirX[i];
+                int nextY = nowY + dirY[i];
+
+                if(i==reverseDir) continue;
+                if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= N) continue;
+                if (house[nextX][nextY] == '*') continue;
+                if(house[nowX][nowY]=='!'){
+                    if(i==nowDir) queue.add(new int[]{nextX, nextY, i, nowMirror}); // (x, y, 방향, 거울 개수)
+                    else  queue.add(new int[]{nextX, nextY, i, nowMirror+1}); // (x, y, 방향, 거울 개수)
+                }
+
+                if(house[nowX][nowY]=='.'&&i==nowDir) {
+                    queue.add(new int[]{nextX, nextY, i, nowMirror}); // (x, y, 방향, 거울 개수)
+                }
+            }
+        }
+
         System.out.println(answer);
     }
 
-    static void findMirror(int x, int y, int count, int exDir) { //# 찾았으면 해당 카운트 return, # 못 찾았으면 -1 return
-        if(room[x][y]=='#') {
-            answer = Math.min(answer, count);
-        }
-
-        for(int i=0; i<4; i++) {
-            int nextX=x+dirX[i];
-            int nextY=y+dirY[i];
-            if(room[nextX][nextY]=='*'||room[nextX][nextY]=='\0'||minRoute[i][x][y]==true) continue;
-            minRoute[i][x][y]=true;
-            if(room[nextX][nextY]=='!') {
-                if(i!=exDir) findMirror(nextX, nextY,count+1, i);
-                else findMirror(nextX, nextY,count, i);
-            }
-            else if(exDir==-1||exDir==i) findMirror(nextX, nextY, count, i);
-            minRoute[i][x][y]=false;
-        }
-    }
 }
