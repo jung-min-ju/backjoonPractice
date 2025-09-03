b4799b134e6d8171cf282ccedea11dc382ebf586
minju <minju9711@naver.com>
2025-09-03 22:10:14 +0900

    b_1461(도서관) 풀이 제출

--- b_1092/src/Main.java
+++ b_1092/src/Main.java
@@ -4,67 +4,78 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-
-    public static int N,M;
-    public static Integer[] crain, box;
-    public static int answer =0 ;
-    public static boolean[] visited;
-    public static int[] crain_positions;
-    public static void main(String[] args) throws IOException{
+    static int N, M;
+    static List<Integer> CRANE=new ArrayList<>();
+    static List<Integer> BOX=new ArrayList<>();
+    static boolean [] USED_CRANE = new boolean[50];
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        crain = new Integer[N];
-
+        N = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        for(int i=0;i<N;i++) {
-            crain[i] = Integer.parseInt(st.nextToken());
+        for(int i=0; i<N; i++){
+            CRANE.add(Integer.parseInt(st.nextToken()));
         }
 
+        M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken());
-        box = new Integer[M];
-
-        st = new StringTokenizer(br.readLine());
-        for(int i=0;i<M;i++) {
-            box[i] = Integer.parseInt(st.nextToken());
+        for(int i=0; i<M; i++){
+            BOX.add(Integer.parseInt(st.nextToken()));
         }
 
-        //내림차순으로 정렬하여야, 큰것들부터 처리합니다. (큰 크레인이 작은것들을 처리하는 일이 없어야만 합니다.)
-        Arrays.sort(crain, Collections.reverseOrder());
-        Arrays.sort(box, Collections.reverseOrder());
+        Collections.sort(CRANE);
+        Collections.sort(BOX);
 
-        if(crain[0] < box[0]) {
-            System.out.println("-1");
-            return;
-        }
+        int time = 0, boxIdx=0;
+        boolean impossible=false;
 
-        crain_positions = new int[N];
-        visited = new boolean[M];
-        //박스를 모두 옮기기전까지 작동
-        while(M > 0) {
-
-            //각 크레인이 순회
-            for(int i=0;i<N;i++) {
-                if(M == 0) break;
-
-                for(int j=crain_positions[i];j<box.length;j++) {
-                    if(visited[j] == true) continue;
-                    if(crain[i] < box[j]) {
-                        crain_positions[i]++;
-                    }
-                    else if(crain[i] >= box[j]) {
-                        visited[j] = true;
-                        M--;
-                        break;
-                    }
+        //현재 박스가 몇 번째 크레인에 들어가야 하는지 확인하기.
+        for(int i=boxIdx; i<M;) {
+            int boxCount=0;
+            for(int j=boxIdx; j<boxIdx+N; j++) {
+                if(BOX.get(boxIdx) > CRANE.get(N-1) || BOX.get(boxIdx) < CRANE.get(0)) {
+                    impossible = true;
+                    break;
                 }
-
+                if(binarySearch(BOX.get(boxIdx))) boxCount++;
             }
-            answer++;
+            if(impossible) break;
+            i+=boxCount;
+            time++;
         }
-        System.out.println(answer);
 
+        System.out.println(impossible ? -1 : time);
     }
+
+    //현재 인덱스로 들어온 박스 무게를, crain list에서 lower bound로 찾기
+    static boolean binarySearch(int value) {
+        int st = 0, ed = N - 1, result = -1;
+
+        while (st <= ed) {
+            int mid = (st + ed) / 2;
+
+            if (CRANE.get(mid) >= value) {
+                result = mid;
+                st = mid + 1; // 내림차순 정렬이라 st 증가
+            } else {
+                ed = mid - 1;
+            }
+        }
+
+        if (result == -1) return false;
+
+        // result부터 왼쪽으로 가면서 아직 안 쓴 크레인 찾기
+        while (result >= 0) {
+            if (!USED_CRANE[result]) {
+                USED_CRANE[result] = true;
+                return true;
+            }
+            result--;
+        }
+
+        return false; // 사용할 수 있는 크레인이 없음
+    }
+
+
 }
--- b_18870/src/main/java/org/example/Main.java
+++ b_18870/src/main/java/org/example/Main.java
@@ -3,52 +3,65 @@ package org.example;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class Main {
-    static int N;
-    static Integer [] original;
-    static Integer [] value;
-    static Map<Integer, Integer> valueMappedNum;
-
+public class Main{
+    static int [] noDupSort;
+    static int END;
     public static void main(String[] args) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        init();
-        valueMappedNum = new HashMap<>();
-
-        //1. 해당 배열 오름차순 정렬
-        Arrays.sort(value);
-
-        //2. HashMap에 초기화
-        int cnt=0;
-        for(int i=0; i<value.length; i++){
-            if(valueMappedNum.containsKey(value[i])) {
-                valueMappedNum.put(value[i], cnt++);
-            }
-        }
-
-        for(int i=0; i<N; i++){
-            sb.append(valueMappedNum.get(original[i])+" ");                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      ````````````````````````````````
-        }
-        System.out.print(sb);
-
-    }
-
-    static void init() throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        N = Integer.parseInt(br.readLine());
-        value = new Integer[N];
-        original = new Integer[N];
-        st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(br.readLine());
+        int [] original = new int[N];
+        int [] sort = new int[N];
+        noDupSort = new int[N];
 
-        for(int i=0; i<N; i++){
-            original[i] = Integer.parseInt(st.nextToken());
-            value[i] = original[i];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            int value = Integer.parseInt(st.nextToken());
+            original[i] = value;
+            sort[i] = value;
         }
+
+        Arrays.sort(sort);
+        noDupSort[0]=sort[0];
+
+        END = 0;
+        for(int i=1; i<N; i++) {
+            if(sort[i]>noDupSort[END]) {
+                noDupSort[++END] = sort[i];
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i : original){
+            sb.append(binarySearch(i)+" ");
+        }
+
+        System.out.println(sb.toString());
+
     }
+
+    static int binarySearch(int value) {
+        int start = 0;
+        int end = END;
+
+        while (end>=start) {
+            int mid = (start+end)/2;
+
+            if(noDupSort[mid]>value) {
+                end=mid-1;
+            } else if (noDupSort[mid]<value){
+                start=mid+1;
+            }
+             else {
+                 return mid;
+            }
+        }
+
+        return 0;
+    }
+
 }
